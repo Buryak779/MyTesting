e07c30cbc5c85c5afec610cc92aae8171963b5b4
buryak779@gmail.com <valik100612>
2018-02-21 10:40:41 +0200

    Form add

--- my_test/admin.py
+++ my_test/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import MyTest, Vote
+from django.contrib import admin
+
+class TestAdmin(admin.ModelAdmin):
+
+    list_display = ("id","question", "my_test")
 
 admin.site.register(MyTest)
-admin.site.register(Vote)
+admin.site.register(Vote,TestAdmin)
 
--- my_test/views.py
+++ my_test/views.py
@@ -2,14 +2,20 @@ from django.shortcuts import render
 from .models import Vote, MyTest
 from my_test.forms import AnswerForm
 
+
 def my_test_all(request):
     return render(request, "my_test/tests_all.html", {"test": MyTest.objects.all})
+
 
 def my_test_single(request, pk):
     questions = Vote.objects.filter(my_test__id=pk)
     if request.method == "POST":
         form = AnswerForm(request.POST)
-
+        if form.is_valid():
+            vote = Vote.objects.get(id=1)
+            answer_true = vote.answer_true
+            out = form.cleaned_data['answer_true']
+            print(out==answer_true)
     else:
         form = AnswerForm()
     return render(request, "my_test/test_single.html", {"questions": questions, "form": form})
